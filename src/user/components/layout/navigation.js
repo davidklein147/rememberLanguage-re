fad9b3800af13be282fc9e7ee29232a151a1a7f6
davidklein147 <davidklein147@gmail.com>
2021-09-14 17:12:41 +0300

    add tasks list

--- src/user/components/layout/navigation.js
+++ src/user/components/layout/navigation.js
@@ -1,27 +1,24 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import HttpRequests from '../../HttpRequests/httpRequests';
 import "./navigation.css";
-import LogButten from './logButton';
 
 
 const Navigation = () => {
-
-
     const actions = [
         {name: "notification", id: 1, lintTo: "#" },
-        {name: "translate",    id: 1, lintTo: "/input" },
-        {name: "input word",   id: 1, lintTo: "#" },
-        {name: "repetition",   id: 1, lintTo: "#" }
+        {name: "translate",    id: 2, lintTo: "#" },
+        {name: "input word",   id: 3, lintTo: "/input" },
+        {name: "repetition",   id: 4, lintTo: "#" }
     ]
 
     return (
-        <ul className="k">
+        <ul className="navAside">
             {actions.map(action => {
                 return (
-                    <li className="navbar-brand ">
-                        <NavLink className={` navLink align-middle`} activeClassName="active"
+                    <li className="navbar-brand">
+                        <NavLink className={`navLink align-middle`} activeClassName="active"
                             to={action.lintTo} key={action.id}>{action.name}</NavLink>
+                            {/* <a href = "/input">{action.name}</a> */}
                     </li>)
             })}
         </ul>
